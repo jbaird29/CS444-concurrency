77448a57adfcd1e6728ed599e9d5205a77f6999e
Jon Baird <jbaird29@gmail.com>
2022-05-18 17:38:06 -0400

    add command line parsing

--- bairdjo_hw4.c
+++ bairdjo_hw4.c
@@ -1,6 +1,50 @@
+#include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
+
+#define INSTRUCTIONS "Instructions:\n\
+-p: run the producer/consumer problem\n\
+  -n {N}: number of producers (required if using -p)\n\
+  -c {C}: number of consumers (required if using -p)\n\
+-d: run the dining philosopher's problem\n\
+-b: run the potion brewers problem\n"
+
+
+int runConsumerProducer(int producers, int consumers) {
+    printf("Producers %d Consumers %d", producers, consumers);
+    return EXIT_SUCCESS;
+}
+
+int runDiningPhilosophers() {
+    printf("Diners!");
+    return EXIT_SUCCESS;
+}
+
+int runBrewMaster() {
+    printf("Brew Masters!");
+    return EXIT_SUCCESS;
+}
+
+int exitAndPrintInstructions() {
+    printf("%s", INSTRUCTIONS);
+    return EXIT_FAILURE;
+}
 
 int main(int argc, char *argv[]) {
-    printf("Hello world.");
-    return 0;
+    if (argc < 2)
+        return exitAndPrintInstructions();
+    // consumer producer problem
+    if(strncmp(argv[1], "-p", 2) == 0) {
+        if(argc < 6)
+            return exitAndPrintInstructions();
+        if(strncmp(argv[2], "-n", 2) != 0 || strncmp(argv[4], "-c", 2) != 0)
+            return exitAndPrintInstructions();
+        return runConsumerProducer(atoi(argv[3]), atoi(argv[5]));
+    } else if (strncmp(argv[1], "-d", 2) == 0) {
+        return runDiningPhilosophers();
+    } else if (strncmp(argv[1], "-b", 2) == 0) {
+        return runBrewMaster();
+    } else {
+        return exitAndPrintInstructions();
+    }
 }
--- makefile
+++ makefile
@@ -1,7 +1,13 @@
 CC=gcc
 
-start: build
-	@./build/bairdjo_hw4
+start_p: build
+	./build/bairdjo_hw4 -p -n $(N) -c $(C)
+
+start_d: build
+	./build/bairdjo_hw4 -d
+
+start_b: build
+	./build/bairdjo_hw4 -b
 
 build: bairdjo_hw4.c
 	@$(CC) --std=gnu99 -Wall -pthread -o ./build/bairdjo_hw4 bairdjo_hw4.c
